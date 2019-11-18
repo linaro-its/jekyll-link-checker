a249b5ed369d8e0c0249168da87f9888b4db1d04
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-11-18 10:20:14 +0000

    Updated the link checker script to take an object oriented approach to enhance readability

--- check_links.py
+++ check_links.py
@@ -23,7 +23,7 @@ import concurrent
 # of filename/link pairs.
 
 
-class CheckLinks:
+class JekyllLinkChecker:
     def __init__(self):
         self.CHROME = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) '
@@ -34,7 +34,61 @@ class CheckLinks:
         self.unique_links = []
         self.failed_links = []
         self.unique_links = []
-        self.self.status_count = 0
+        self.status_count = 0
+        self.html_cache_results = {}
+        self.dns_skip = []
+        self.verbose = 0
+        self.output_file = None
+        self.args = self.parse_args()
+        self.main()
+
+    def main(self):
+        print("Linaro Link Checker")
+        if self.args.verbose is not None:
+            self.verbose = self.args.verbose
+            print("Verbosity is at level %s" % self.verbose)
+        if self.args.skip_dns_check is not None:
+            print("Loading FQDN skip list from %s" % self.args.skip_dns_check)
+            try:
+                self.dns_skip = list(open(self.args.skip_dns_check))
+            except Exception as exception:
+                print("Couldn't load FQDN skip list:", exception)
+        if self.args.output is not None:
+            self.output_file = self.args.output
+        if self.args.directory is not None:
+            print("Scanning '%s'" % self.args.directory)
+            os.chdir(self.args.directory)
+        if self.args.nointernal:
+            print("Skipping internal link checking")
+        if self.args.noexternal:
+            print("Skipping external link checking")
+        # For now, assume that we're just scanning the current directory. Add code
+        # for file paths and possibly URLs at a future date ...
+        self.scan_directory("./", self.args.skip_path)
+
+    def parse_args(self):
+        parser = argparse.ArgumentParser(
+            description="Scan for broken links")
+        parser.add_argument('-d', '--directory', nargs='?', default=None,
+                            help='specifies the directory to scan')
+        parser.add_argument('--skip-dns-check', nargs='?', default=None,
+                            help='specifies text file of FQDNs to skip the DNS '
+                            'check on')
+        parser.add_argument('-s', '--skip-path', action='append',
+                            help='specifies a path to skip when checking URLs')
+        parser.add_argument('-v', '--verbose', action='count')
+        parser.add_argument('-f', '--file', action='append',
+                            help=('specifies a file to check;'
+                                  ' all non-specified files are ignored'))
+        parser.add_argument('--nointernal', action='store_true',
+                            help='skips checking of internal references')
+        parser.add_argument('--noexternal', action='store_true',
+                            help='skips checking of external references')
+        parser.add_argument('-o', '--output', nargs='?', default=None,
+                            help='specifies output file for error results')
+        parser.add_argument('--no-external-errors', action='store_true',
+                            help='ignores errors caused by external broken links')
+        return parser.parse_args()
 
     def drop_dot(self, foo):
         if foo != "" and foo[0] == '.':
@@ -48,7 +102,7 @@ class CheckLinks:
                 if name.endswith((".html", ".htm")):
                     f = os.path.join(root, name)
                     if f not in result:
-                        if verbose >= 3:
+                        if self.verbose >= 3:
                             print("File scan: adding '%s'" % f)
                         result.append(f)
             for d in dirs:
@@ -56,7 +110,7 @@ class CheckLinks:
                 if files_in_d:
                     for f in files_in_d:
                         if f not in result:
-                            if verbose >= 3:
+                            if self.verbose >= 3:
                                 print("File scan: adding '%s'" % f)
                             result.append(f)
         return result
@@ -91,7 +145,7 @@ class CheckLinks:
         # If we're looking at a directory, make sure there is an index file in it.
         if combined_path[-1] == '/':
             combined_path += "index.html"
-        if verbose >= 2:
+        if self.verbose >= 2:
             print(("Validating file: constituent parts are '%s' and '%s',"
                    " combined path is '%s'") % (head, text, combined_path))
         # needs to be a file or directory ...
@@ -123,17 +177,17 @@ class CheckLinks:
                 text = "https:" + text
             # Check the URL to see if it is a web link - that is all we check.
             o = urlparse(text)
-            if not args.noexternal and (o.scheme == "http" or o.scheme == "https"):
+            if not self.args.noexternal and (o.scheme == "http" or o.scheme == "https"):
                 # We use "self.file_link_pairs" to track which files reference which
                 # URLs - we only check URLs *once* but then flag up all
                 # refernces to the link.
                 if [filename, text] not in self.file_link_pairs:
                     self.file_link_pairs.append([filename, text])
                 # ... only check the links once!
-                if text not in unique_links:
-                    unique_links.append(text)
+                if text not in self.unique_links:
+                    self.unique_links.append(text)
                 return None  # Postpone the decision for now ...
-            elif not args.nointernal and o.scheme == "":
+            elif not self.args.nointernal and o.scheme == "":
                 return self.validate_file_link(filename, text)
             # If skipping stuff, return the answer of no problems ...
             return None
@@ -151,9 +205,9 @@ class CheckLinks:
     async def async_check_link(self, session, url):
         # Check that the host resolves, but only if it isn't in the DNS skip list
         parts = urlparse(url)
-        if parts.netloc not in dns_skip:
+        if parts.netloc not in self.dns_skip:
             try:
-                foo = socket.gethostbyname(parts.netloc)  # noqa
+                socket.gethostbyname(parts.netloc)  # noqa
             except socket.gaierror as err:
                 return self.output_status('D', 1)
         # Now try to validate the URL
@@ -177,7 +231,7 @@ class CheckLinks:
                             response.status > 499):
                         return self.output_status('.', 0)
                     else:
-                        if verbose >= 3:
+                        if self.verbose >= 3:
                             print(response.status, response.url)
                         # We only really care about full-on failures, i.e. 404.
                         # Other status codes can be returned just because we aren't
@@ -213,11 +267,11 @@ class CheckLinks:
         # will point to the corresponding result.
         i = 0
         for l in links:
-            if l not in html_cache_results:
+            if l not in self.html_cache_results:
                 if results[i] == 0:
-                    html_cache_results[l] = None
+                    self.html_cache_results[l] = None
                 elif results[i] > 0:
-                    html_cache_results[l] = "%s [%d]" % (l, results[i])
+                    self.html_cache_results[l] = "%s [%d]" % (l, results[i])
             i += 1
 
     # Perform an async check of all of the web links we've collected then
@@ -227,7 +281,7 @@ class CheckLinks:
         self.status_count = 1
 
         web_failed_links = []
-        print("Checking %s web links ..." % len(unique_links))
+        print("Checking %s web links ..." % len(self.unique_links))
         # Force IPv4 only to avoid
         # https://stackoverflow.com/questions/40347726/python-3-5-asyincio-and-aiohttp-errno-101-network-is-unreachable
         conn = aiohttp.TCPConnector(
@@ -237,12 +291,12 @@ class CheckLinks:
         )
         async with aiohttp.ClientSession(connector=conn,
                                          conn_timeout=60) as session:
-            await async_check_web(session, unique_links)
+            await self.async_check_web(session, self.unique_links)
         for p in self.file_link_pairs:
             # p[0] is the file path and p[1] is the URL.
-            if (p[1] in html_cache_results and
-                    html_cache_results[p[1]] is not None):
-                error = [p[0], html_cache_results[p[1]]]
+            if (p[1] in self.html_cache_results and
+                    self.html_cache_results[p[1]] is not None):
+                error = [p[0], self.html_cache_results[p[1]]]
                 if error not in web_failed_links:
                     web_failed_links.append(error)
         return web_failed_links
@@ -251,7 +305,7 @@ class CheckLinks:
 
     def check_file(self, filename, skip_list):
         file_failed_links = []
-        if not matched_skip(filename, skip_list):
+        if not self.matched_skip(filename, skip_list):
             try:
                 with open(filename, "r") as myfile:
                     data = myfile.read()
@@ -266,7 +320,7 @@ class CheckLinks:
                 for g in gh_links:
                     a_links.remove(g)
                 for link in a_links:
-                    result = validate_link(filename, link.get('href'))
+                    result = self.validate_link(filename, link.get('href'))
                     if result is not None:
                         error = [filename, result]
                         if error not in file_failed_links:
@@ -274,7 +328,7 @@ class CheckLinks:
                 # Check linked images
                 img_links = soup.find_all('img')
                 for link in img_links:
-                    result = validate_link(filename, link.get('src'))
+                    result = self.validate_link(filename, link.get('src'))
                     if result is not None:
                         error = [filename, result]
                         if error not in file_failed_links:
@@ -285,35 +339,33 @@ class CheckLinks:
 
     def failures_to_dict(self, list_of_failures):
         failure_dict = {}
-        for f in list_of_failures:
-            file = drop_dot(f[0])
-            url = drop_dot(f[1])
-            if file in failure_dict:
-                failure_dict[file].append(url)
+        for failure in list_of_failures:
+            failed_file = self.drop_dot(failure[0])
+            url = self.drop_dot(failure[1])
+            if failed_file in failure_dict:
+                failure_dict[failed_file].append(url)
             else:
-                failure_dict[file] = [url]
+                failure_dict[failed_file] = [url]
         return failure_dict
 
     # Scan the specified directory, ignoring anything that matches skip_list.
 
     def scan_directory(self, path, skip_list):
-
         soft_failure = False
-
         count = 1
         html_files = self.get_all_html_files(path)
         total = len(html_files)
-        if args.file is not None:
-            total = len(args.file)
+        if self.args.file is not None:
+            total = len(self.args.file)
         for hf in html_files:
-            if args.file is None or hf in args.file:
+            if self.args.file is None or hf in self.args.file:
                 print("(%s/%s) Checking '%s'" % (count, total, hf))
                 count += 1
-                results = check_file(hf, skip_list)
+                results = self.check_file(hf, skip_list)
                 for r in results:
-                    if r not in failed_links:
-                        failed_links.append(r)
-        if len(unique_links) == 0:
+                    if r not in self.failed_links:
+                        self.failed_links.append(r)
+        if len(self.unique_links) == 0:
             print("No web links to check.")
         else:
             loop = asyncio.new_event_loop()
@@ -322,7 +374,7 @@ class CheckLinks:
             loop.close()
             # If we are NOT reporting broken external links as an error,
             # report them as warnings if there are any.
-            if args.no_external_errors:
+            if self.args.no_external_errors:
                 if cul_result != []:
                     print("\n\nWARNING! %s failed external links have been "
                           "found:\n" % len(cul_result))
@@ -332,17 +384,18 @@ class CheckLinks:
                 # Can do a simple append here because these are all web failures
                 # and so don't need to check if the failure already exists in the
                 # list.
-                failed_links += cul_result
-        if failed_links != []:
-            if output_file is not None:
+                self.failed_links += cul_result
+        if self.failed_links != []:
+            if self.output_file is not None:
                 save_out = sys.stdout
-                fsock = open(output_file, 'w')
+                fsock = open(self.output_file, 'w')
                 sys.stdout = fsock
             else:
                 print("")
-            print("%s failed links have been found:\n" % len(failed_links))
-            self.report_failed_links(failed_links)
-            if output_file is not None:
+            print("%s failed links have been found:\n" %
+                  len(self.failed_links))
+            self.report_failed_links(self.failed_links)
+            if self.output_file is not None:
                 sys.stdout = save_out
                 fsock.close()
             sys.exit(1)
@@ -360,52 +413,5 @@ class CheckLinks:
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Scan for broken links")
-    parser.add_argument('-d', '--directory', nargs='?', default=None,
-                        help='specifies the directory to scan')
-    parser.add_argument('--skip-dns-check', nargs='?', default=None,
-                        help='specifies text file of FQDNs to skip the DNS '
-                        'check on')
-    parser.add_argument('-s', '--skip-path', action='append',
-                        help='specifies a path to skip when checking URLs')
-    parser.add_argument('-v', '--verbose', action='count')
-    parser.add_argument('-f', '--file', action='append',
-                        help=('specifies a file to check;'
-                              ' all non-specified files are ignored'))
-    parser.add_argument('--nointernal', action='store_true',
-                        help='skips checking of internal references')
-    parser.add_argument('--noexternal', action='store_true',
-                        help='skips checking of external references')
-    parser.add_argument('-o', '--output', nargs='?', default=None,
-                        help='specifies output file for error results')
-    parser.add_argument('--no-external-errors', action='store_true',
-                        help='ignores errors caused by external broken links')
-    args = parser.parse_args()
-    html_cache_results = {}
-    dns_skip = []
-    verbose = 0
-    output_file = None
 
-    print("Linaro Link Checker")
-
-    if args.verbose is not None:
-        verbose = args.verbose
-        print("Verbosity is at level %s" % verbose)
-    if args.skip_dns_check is not None:
-        print("Loading FQDN skip list from %s" % args.skip_dns_check)
-        try:
-            dns_skip = list(open(args.skip_dns_check))
-        except Exception as exception:
-            print("Couldn't load FQDN skip list")
-    if args.output is not None:
-        output_file = args.output
-    if args.directory is not None:
-        print("Scanning '%s'" % args.directory)
-        os.chdir(args.directory)
-    if args.nointernal:
-        print("Skipping internal link checking")
-    if args.noexternal:
-        print("Skipping external link checking")
-    # For now, assume that we're just scanning the current directory. Add code
-    # for file paths and possibly URLs at a future date ...
-    scan_directory("./", args.skip_path)
+    link_checker = JekyllLinkChecker()
