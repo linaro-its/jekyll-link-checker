7c6058e1425b8bc65a13a37d33f4d28243370ef8
Philip Colmer <philip.colmer@linaro.org>
2020-12-18 14:46:23 +0000

    GitHub now, not Bamboo

--- check-links.sh
+++ check-links.sh
@@ -8,20 +8,21 @@ function get_tag_for_latest(){
     # From https://stackoverflow.com/a/41830007/1233830
     REPOSITORY="linaroits/linkcheck"
     TARGET_TAG="latest"
-    # check that we have Internet access - bail quickly if we don't
-    curl -Is "https://auth.docker.io" >/dev/null 2>&1 || return $?
-    # get authorization token
+    # Check that we have Internet access - bail quickly if we don't.
+    # We don't care about what curl returns so throw it away.
+    _=$(curl -s "https://auth.docker.io") || return $?
+    # Get authorization token.
     TOKEN=$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:$REPOSITORY:pull" | jq -r .token) || return $?
-    # find all tags
+    # Find all tags.
     ALL_TAGS=$(curl -s -H "Authorization: Bearer $TOKEN" https://index.docker.io/v2/$REPOSITORY/tags/list | jq -r .tags[]) || return $?
-    # get image digest for target
-    TARGET_DIGEST=$(curl -s -D - -H "Authorization: Bearer $TOKEN" -H "Accept: application/vnd.docker.distribution.manifest.v2+json" https://index.docker.io/v2/$REPOSITORY/manifests/$TARGET_TAG | grep Docker-Content-Digest | cut -d ' ' -f 2) || return $?
+    # Get image digest for target.
+    TARGET_DIGEST=$(curl -s -D - -H "Authorization: Bearer $TOKEN" -H "Accept: application/vnd.docker.distribution.manifest.v2+json" "https://index.docker.io/v2/$REPOSITORY/manifests/$TARGET_TAG" | grep Docker-Content-Digest | cut -d ' ' -f 2) || return $?
     # Iterate through the tags, but we need to use unquoted bash expansion so turn off the shellcheck warning
     # shellcheck disable=SC2068
     for tag in ${ALL_TAGS[@]}; do
         # get image digest
         digest=$(curl -s -D - -H "Authorization: Bearer $TOKEN" -H "Accept: application/vnd.docker.distribution.manifest.v2+json" "https://index.docker.io/v2/$REPOSITORY/manifests/$tag" | grep Docker-Content-Digest | cut -d ' ' -f 2) || return $?
-        # check digest
+        # and check digest.
         if [ "$TARGET_DIGEST" = "$digest" ] && [ "$tag" != "$TARGET_TAG" ]; then
             LATEST_ALIAS="$tag"
         fi
@@ -31,7 +32,7 @@ function get_tag_for_latest(){
 #
 # If possible, show which container version this is
 if [ -n "${BAMBOO_BUILD}" ]; then
-    echo "Container built by bamboo.linaro.org: ${BAMBOO_BUILD}"
+    echo "Container built by GitHub. Build reference: ${BAMBOO_BUILD}"
     get_tag_for_latest || LATEST_ALIAS=""
     if [ -n "$LATEST_ALIAS" ] && [ "$LATEST_ALIAS" != "${BAMBOO_BUILD}" ]; then
         echo "************************************************************"
